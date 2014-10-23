f08e18df5af0062468d1f6e72d5f40e7b1429a01
xie <320542915@qq.com>
2014-10-23 20:38:51 +0800

    Initial Commit

---
+++ XLWeibo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XLWeibo
+//
+//  Created by admin on 14-10-23.
+//  Copyright (c) 2014年 xl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XLWeibo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XLWeibo
+//
+//  Created by admin on 14-10-23.
+//  Copyright (c) 2014年 xl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
